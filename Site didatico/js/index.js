96798bc3facf21e4a40d9c0353e503db60015489
Garotomuleque <119128013+Garotomuleque@users.noreply.github.com>
2024-10-14 10:37:26 -0300

    Add files via upload

---
+++ Site didatico/js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("Icone").addEventListener("click",function(){
+    history.back();
+});
+
+document.getElementById("Login").addEventListener("click",function(){
+    document.getElementById("conteiner").style.visibility="visible";
+});
